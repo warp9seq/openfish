c3d81b3c31675aeff08d0af74ac51d454a842672
Bonson Wong <boson.ym@gmail.com>
2024-08-29 13:06:09 +1000

    tests for each model

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,9 @@ int main(int argc, char* argv[]) {
     torch::Tensor scores;
     torch::load(scores, argv[1]);
     const CRFModelConfig config = load_crf_model_config(argv[2]);
+    const int num_chunks = strtol(argv[3], NULL, 10);
     
     const int target_threads = 40;
-    const int num_chunks = 1000;
     const DecoderOptions options = DecoderOptions();
     const int runner_idx = 0;
 
