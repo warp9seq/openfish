b8d1f4cf9dd0bfedfd4713742b4958f4c37ee838
Bonson Wong <boson.ym@gmail.com>
2024-09-06 17:28:32 +1000

    fix test

--- test/extensive.sh
+++ test/extensive.sh
@@ -46,10 +46,10 @@ quick_test() {
 
     ex ./openfish ${SCORES} models/dna_r10.4.1_e8.2_400bps_${MODEL}@v4.2.0 ${BATCH_SIZE} ${STATE_LEN} || die "tool failed"
 
-    diff ${DATA_DIR}/${MODEL}_${BATCH_SIZE}c_scores_TNC.pt scores_TNC.pt || die "failed diff"
-    diff ${DATA_DIR}/${MODEL}_${BATCH_SIZE}c_bwd_NTC.pt bwd_NTC.pt || die "failed diff"
-    diff ${DATA_DIR}/${MODEL}_${BATCH_SIZE}c_fwd_NTC.pt fwd_NTC.pt || die "failed diff"
-    diff ${DATA_DIR}/${MODEL}_${BATCH_SIZE}c_post_NTC.pt post_NTC.pt || die "failed diff"
+    diff ${DATA_DIR}/${MODEL}_${BATCH_SIZE}c_scores_TNC.blob scores_TNC.blob || die "failed diff"
+    diff ${DATA_DIR}/${MODEL}_${BATCH_SIZE}c_bwd_NTC.blob bwd_NTC.blob || die "failed diff"
+    diff ${DATA_DIR}/${MODEL}_${BATCH_SIZE}c_fwd_NTC.blob fwd_NTC.blob || die "failed diff"
+    diff ${DATA_DIR}/${MODEL}_${BATCH_SIZE}c_post_NTC.blob post_NTC.blob || die "failed diff"
 
     echo "tests passed for ${MODEL}"
 }
