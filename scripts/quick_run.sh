69ff1b5c2e6f32258fd7296d004e766ac1f30498
Bonson Wong <boson.ym@gmail.com>
2024-09-23 13:32:09 +1000

    proper gpu testing

--- scripts/quick_run.sh
+++ scripts/quick_run.sh
@@ -33,7 +33,7 @@ SCORES=${DATA_DIR}/${MODEL}_${BATCH_SIZE}c_scores_TNC.blob
 /usr/bin/time  --verbose ./openfish ${SCORES} models/dna_r10.4.1_e8.2_400bps_${MODEL}@v4.2.0 ${BATCH_SIZE} ${STATE_LEN} || die "tool failed"
 
 # diff ${DATA_DIR}/${MODEL}_${BATCH_SIZE}c_scores_TNC.blob scores_TNC.blob || die "failed diff"
-diff ${DATA_DIR}/${MODEL}_${BATCH_SIZE}c_bwd_NTC.blob bwd_NTC.blob || die "failed diff"
+# diff ${DATA_DIR}/${MODEL}_${BATCH_SIZE}c_bwd_NTC.blob bwd_NTC.blob || die "failed diff"
 # diff ${DATA_DIR}/${MODEL}_${BATCH_SIZE}c_fwd_NTC.blob fwd_NTC.blob || die "failed diff"
 # diff ${DATA_DIR}/${MODEL}_${BATCH_SIZE}c_post_NTC.blob post_NTC.blob || die "failed diff"
 
