c2608869332f5654b7970eaaf0a6ff9fa31d11d2
Bonson Wong <boson.ym@gmail.com>
2024-09-23 15:10:57 +1000

    done scans

--- scripts/gpu_quick_run.sh
+++ scripts/gpu_quick_run.sh
@@ -38,8 +38,10 @@ SCORES=${DATA_DIR}/${MODEL}_${BATCH_SIZE}c_scores_TNC.blob
 /usr/bin/time  --verbose ./openfish ${SCORES} models/dna_r10.4.1_e8.2_400bps_${MODEL}@v4.2.0 ${BATCH_SIZE} ${STATE_LEN} || die "tool failed"
 
 # ./compare_blob ${DATA_DIR}/${MODEL}_${BATCH_SIZE}c_scores_TNC.blob scores_TNC.blob $TENS_LEN || die "failed diff"
+echo "comparing bwd tensors..."
 ./compare_blob ${DATA_DIR}/${MODEL}_${BATCH_SIZE}c_bwd_NTC.blob bwd_NTC.blob $TENS_LEN || die "failed diff"
-# ./compare_blob ${DATA_DIR}/${MODEL}_${BATCH_SIZE}c_fwd_NTC.blob fwd_NTC.blob $TENS_LEN || die "failed diff"
+echo "comparing post tensors..."
+./compare_blob ${DATA_DIR}/${MODEL}_${BATCH_SIZE}c_post_NTC.blob post_NTC.blob $TENS_LEN || die "failed diff"
 # ./compare_blob ${DATA_DIR}/${MODEL}_${BATCH_SIZE}c_post_NTC.blob post_NTC.blob $TENS_LEN || die "failed diff"
 
 # echo "tests passed for ${MODEL}"
