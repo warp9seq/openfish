121117ce131590af68ffce32d65f7dd7e5b34003
Bonson Wong <boson.ym@gmail.com>
2024-09-23 17:42:51 +1000

    added compiler flags for cuda

---
+++ src/openfish.cpp
@@ -0,0 +1,18 @@
+#include "openfish.h"
+
+void decode(
+    const int T,
+    const int N,
+    const int C,
+    const int target_threads,
+    float *scores_TNC,
+    std::vector<DecodedChunk>& chunk_results,
+    const int state_len,
+    const DecoderOptions* options
+) {
+#ifdef HAVE_CUDA
+    decode_gpu(T, N, C, target_threads, scores_TNC, chunk_results, state_len, options);
+#else
+    decode_cpu(T, N, C, target_threads, scores_TNC, chunk_results, state_len, options);
+#endif
+}
