c1f4e219135aeec28f53eb91bd97bffc9b2c946c
Bonson Wong <boson.ym@gmail.com>
2024-10-14 18:49:18 +1100

    remove duplicate struct defs

--- src/decode.h
+++ src/decode.h
@@ -2,12 +2,46 @@
 #define DECODE_H
 
 #include <stdint.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 typedef int32_t state_t;
+
+typedef struct beam_element {
+    state_t state;
+    uint8_t prev_element_index;
+    bool stay;
+} beam_element_t;
+
+typedef struct beam_front_element {
+    uint32_t hash;
+    state_t state;
+    uint8_t prev_element_index;
+    bool stay;
+} beam_front_element_t;
+
+typedef struct beam_args {
+    float *scores_TNC;
+    float *bwd_NTC;
+    float *post_NTC;
+    size_t T;
+    size_t N;
+    size_t C;
+    int num_state_bits;
+} beam_args_t;
+
+typedef struct scan_args {
+    float *scores_in;
+    uint64_t num_states;
+    uint64_t T;
+    uint64_t N;
+    uint64_t C;
+    float fixed_stay_score;
+} scan_args_t;
 
 #define NUM_BASE_BITS (2)
 #define NUM_BASES (4)
