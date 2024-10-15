32972e4a786bdff9a01bdb852f5b01a8e326eb0a
Bonson Wong <boson.ym@gmail.com>
2024-10-16 10:15:29 +1100

    switch to debug log

--- src/decode_cuda.cu
+++ src/decode_cuda.cu
@@ -31,8 +31,8 @@ void decode_cuda(
         grid_len *= 2;
     }
 
-    fprintf(stderr, "chosen block_dims: %d x %d for num_states %d\n", block_width, block_width, num_states);
-    fprintf(stderr, "chosen grid_len: %d for batch size %d\n", grid_len, N);
+    OPENFISH_LOG_DEBUG("chosen block_dims: %d x %d for num_states %d\n", block_width, block_width, num_states);
+    OPENFISH_LOG_DEBUG("chosen grid_len: %d for batch size %d\n", grid_len, N);
 
     double t0, t1, elapsed;
     dim3 block_size(block_width, block_width, 1);
@@ -42,7 +42,7 @@ void decode_cuda(
     
     const uint64_t num_scan_elem = N * (T + 1) * num_states;
 
-    OPENFISH_LOG_TRACE("scores tensor dim: %d, %d, %d", T, N, C);
+    OPENFISH_LOG_DEBUG("scores tensor dim: %d, %d, %d", T, N, C);
 
 #ifdef DEBUG
     float *bwd_NTC = (float *)malloc(num_scan_elem * sizeof(float));
@@ -82,7 +82,7 @@ void decode_cuda(
     if (num_states == 64) n_batch = 140; // fast
     else if (num_states == 256) n_batch = 345; // hac
     else if (num_states == 1024) n_batch = 685; // sup
-    fprintf(stderr, "simulating %d batches...\n", n_batch);
+    OPENFISH_LOG_DEBUG("simulating %d batches...\n", n_batch);
 #endif
 
     // bwd scan
@@ -98,7 +98,7 @@ void decode_cuda(
 	// end timing
 	t1 = realtime();
     elapsed = t1 - t0;
-    fprintf(stderr, "bwd scan completed in %f secs\n", elapsed);
+    OPENFISH_LOG_DEBUG("bwd scan completed in %f secs\n", elapsed);
     
     // fwd + post scan
 	t0 = realtime();
@@ -113,7 +113,7 @@ void decode_cuda(
 	// end timing
 	t1 = realtime();
     elapsed = t1 - t0;
-    fprintf(stderr, "fwd scan completed in %f secs\n", elapsed);
+    OPENFISH_LOG_DEBUG("fwd scan completed in %f secs\n", elapsed);
 
     // beam search
 
@@ -212,7 +212,7 @@ void decode_cuda(
 	// end timing
 	t1 = realtime();
     elapsed = t1 - t0;
-    fprintf(stderr, "beam search completed in %f secs\n", elapsed);
+    OPENFISH_LOG_DEBUG("beam search completed in %f secs\n", elapsed);
 
     t0 = realtime();
 #ifdef BENCH
@@ -231,7 +231,7 @@ void decode_cuda(
 	// end timing
 	t1 = realtime();
     elapsed = t1 - t0;
-    fprintf(stderr, "compute quality data completed in %f secs\n", elapsed);
+    OPENFISH_LOG_DEBUG("compute quality data completed in %f secs\n", elapsed);
 
     t0 = realtime();
 #ifdef BENCH
@@ -256,7 +256,7 @@ void decode_cuda(
 	// end timing
 	t1 = realtime();
     elapsed = t1 - t0;
-    fprintf(stderr, "generate sequence completed in %f secs\n", elapsed);
+    OPENFISH_LOG_DEBUG("generate sequence completed in %f secs\n", elapsed);
 
     // copy beam_search results
     cudaMemcpy(*moves, moves_gpu, sizeof(uint8_t) * N * T, cudaMemcpyDeviceToHost);
