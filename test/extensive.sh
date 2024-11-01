7e01bd56cc8baab4313aefcc3385aa9418d2e3fa
Bonson Wong <boson.ym@gmail.com>
2024-11-01 14:36:17 +1100

    added mem check options to extensive tests

--- test/extensive.sh
+++ test/extensive.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DATA_DIR=/data/bonwon/slorado_test_data/tensors
+DATA_DIR="/data/bonwon/slorado_test_data/blobs"
 
 # terminate script
 die() {
@@ -9,15 +9,33 @@ die() {
 	exit 1
 }
 
+gpu=0
+
 if [ "$1" = 'mem' ]; then
     mem=1
 else
     mem=0
 fi
 
+if [ "$1" = 'mem_gpu' ]; then
+    mem=1
+    gpu=1
+else
+    mem=0
+fi
+
+if [ "$1" = 'cuda' ]; then
+    cuda=1
+    gpu=1
+else
+    cuda=0
+fi
+
 ex() {
     if [ $mem -eq 1 ]; then
-        valgrind --leak-check=full --error-exitcode=1 "$@"
+        /install/valgrind-3.21/bin/valgrind --error-exitcode=1  --leak-check=full --show-leak-kinds=all --suppressions=test/valgrind.supp --gen-suppressions=yes "$@"
+    elif [ $cuda -eq 1 ]; then
+        cuda-memcheck --leak-check full "$@"
     else
         "$@"
     fi
@@ -41,17 +59,13 @@ quick_test() {
         STATE_LEN=5
     fi
 
-    DATA_DIR=/data/bonwon/slorado_test_data/blobs
+    # warning: if the gpu decoder takes in full float values its not going to panic
     SCORES=${DATA_DIR}/${MODEL}_${BATCH_SIZE}c_scores_TNC.blob
+    if [ $gpu -eq 1 ]; then
+        SCORES=${DATA_DIR}/${MODEL}_${BATCH_SIZE}c_scores_TNC_half.blob
+    fi
 
     ex ./openfish ${SCORES} models/dna_r10.4.1_e8.2_400bps_${MODEL}@v4.2.0 ${BATCH_SIZE} ${STATE_LEN} || die "tool failed"
-
-    diff ${DATA_DIR}/${MODEL}_${BATCH_SIZE}c_scores_TNC.blob scores_TNC.blob || die "failed diff"
-    diff ${DATA_DIR}/${MODEL}_${BATCH_SIZE}c_bwd_NTC.blob bwd_NTC.blob || die "failed diff"
-    diff ${DATA_DIR}/${MODEL}_${BATCH_SIZE}c_fwd_NTC.blob fwd_NTC.blob || die "failed diff"
-    diff ${DATA_DIR}/${MODEL}_${BATCH_SIZE}c_post_NTC.blob post_NTC.blob || die "failed diff"
-
-    echo "tests passed for ${MODEL}"
 }
 
 quick_test fast || die "running the tool failed"
