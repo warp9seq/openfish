52fa75fdf84f7d2124d3ce9eb4d7e54b3f58e9b7
Bonson Wong <boson.ym@gmail.com>
2024-11-02 10:42:20 +1100

    update extensive test

--- test/extensive.sh
+++ test/extensive.sh
@@ -10,30 +10,12 @@ die() {
 }
 
 gpu=0
-
-if [ "$1" = 'mem' ]; then
-    mem=1
-else
-    mem=0
-fi
-
-if [ "$1" = 'mem_gpu' ]; then
-    mem=1
-    gpu=1
-else
-    mem=0
-fi
-
-if [ "$1" = 'cuda' ]; then
-    cuda=1
-    gpu=1
-else
-    cuda=0
-fi
+mem=0
+cuda=0
 
 ex() {
     if [ $mem -eq 1 ]; then
-        valgrind --error-exitcode=1  --leak-check=full --show-leak-kinds=all --suppressions=test/valgrind.supp --gen-suppressions=yes "$@"
+        valgrind --error-exitcode=1 --leak-check=full --show-leak-kinds=all --suppressions=test/valgrind.supp "$@"
     elif [ $cuda -eq 1 ]; then
         cuda-memcheck --leak-check full "$@"
     else
@@ -43,6 +25,20 @@ ex() {
 
 quick_test() {
     MODEL=$1
+
+    if [ "$2" = 'mem' ]; then
+        mem=1
+    fi
+
+    if [ "$2" = 'mem_gpu' ]; then
+        mem=1
+        gpu=1
+    fi
+
+    if [ "$2" = 'cuda' ]; then
+        cuda=1
+        gpu=1
+    fi
 
     STATE_LEN=3
     BATCH_SIZE=1000
@@ -68,8 +64,20 @@ quick_test() {
     ex ./openfish ${SCORES} models/dna_r10.4.1_e8.2_400bps_${MODEL}@v4.2.0 ${BATCH_SIZE} ${STATE_LEN} || die "tool failed"
 }
 
-quick_test fast || die "running the tool failed"
-quick_test hac || die "running the tool failed"
-quick_test sup || die "running the tool failed"
+make clean && make
+
+quick_test fast mem || die "running the tool failed"
+quick_test hac mem || die "running the tool failed"
+quick_test sup mem || die "running the tool failed"
+
+make clean && make cuda=1
+
+quick_test fast mem_gpu || die "running the tool failed"
+quick_test hac mem_gpu || die "running the tool failed"
+quick_test sup mem_gpu || die "running the tool failed"
+
+quick_test fast cuda || die "running the tool failed"
+quick_test hac cuda || die "running the tool failed"
+quick_test sup cuda || die "running the tool failed"
 
 echo "tests passed"
