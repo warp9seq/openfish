fa1209b5084c50ae82b79acf7089c73b63cb502c
Hasindu Gamaarachchi <hasindu2008@gmail.com>
2024-11-11 17:51:59 +1100

    a few error checks

--- src/main.c
+++ src/main.c
@@ -6,6 +6,7 @@
 #include <math.h>
 #include <string.h>
 #include <stdlib.h>
+#include <assert.h>
 
 #if defined HAVE_CUDA
 #include "decode_cuda.h"
@@ -16,13 +17,22 @@
 #endif
 
 int main(int argc, char* argv[]) {
+
+    if (argc != 5) {
+        fprintf(stderr,"Usage: %s <scores.blob> <MODEL> <BATCH_SIZE> <STATE_LEN>\n", argv[0]);
+        fprintf(stderr,"e.g. %s test/blobs/fast_1000c_scores_TNC.blob models/dna_r10.4.1_e8.2_400bps_fast@v4.2.0 1000 3\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
     const int T = 1666;
     const int N = strtol(argv[3], NULL, 10);
+    assert(N > 0);
     const int state_len = strtol(argv[4], NULL, 10);
+    assert(state_len > 0);
     const int C = pow(4, state_len) * 4;
 
     set_openfish_log_level(OPENFISH_LOG_DBUG);
-    
+
     size_t scores_len = T * N * C;
 #if defined HAVE_CUDA || defined HAVE_HIP
     const int elem_size = sizeof(uint16_t);
@@ -34,15 +44,16 @@ int main(int argc, char* argv[]) {
     MALLOC_CHK(scores);
 
     FILE *fp = fopen(argv[1], "rb");
+    F_CHK(fp, argv[1]);
 
     size_t result = fread(scores, elem_size, scores_len, fp);
     if (result != scores_len) {
-        OPENFISH_ERROR("%s", "error reading score file");
+        OPENFISH_ERROR("%s: %s", "error reading score file", strerror(errno));
         exit(EXIT_FAILURE);
     }
     fclose(fp);
-    
-    
+
+
     openfish_opt_t options = DECODER_INIT;
 
     // config mods from 4.2.0 models
@@ -56,7 +67,7 @@ int main(int argc, char* argv[]) {
         options.q_scale = 0.95;
         options.q_shift = 0.5;
     }
-    
+
     uint8_t *moves;
     char *sequence;
     char *qstring;
@@ -73,21 +84,34 @@ int main(int argc, char* argv[]) {
 #endif
 
     fp = fopen("moves.blob", "w");
-    fwrite(moves, sizeof(uint8_t), N * T, fp);
+    F_CHK(fp, "moves.blob");
+    if(fwrite(moves, sizeof(uint8_t), N * T, fp) != N * T) {
+        fprintf(stderr, "error writing moves file: %s\n",strerror(errno));
+        exit(EXIT_FAILURE);
+    }
     fclose(fp);
 
     fp = fopen("sequence.blob", "w");
-    fwrite(sequence, sizeof(char), N * T, fp);
+    F_CHK(fp, "sequence.blob");
+    if(fwrite(sequence, sizeof(char), N * T, fp) != N * T) {
+        fprintf(stderr, "error writing sequence file: %s\n",strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
     fclose(fp);
 
     fp = fopen("qstring.blob", "w");
-    fwrite(qstring, sizeof(char), N * T, fp);
+    F_CHK(fp, "qstring.blob");
+    if (fwrite(qstring, sizeof(char), N * T, fp) != N * T) {
+        fprintf(stderr, "error writing qstring file: %s\n",strerror(errno));
+        exit(EXIT_FAILURE);
+    }
     fclose(fp);
 
     free(moves);
     free(sequence);
     free(qstring);
-    
+
     free(scores);
 
 #if defined DEBUG && defined HAVE_CUDA
