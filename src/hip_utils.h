719d2ca647fe33a4f9dcb571e7100ee89f62dbae
Bonson Wong <boson.ym@gmail.com>
2024-11-11 19:11:10 +1100

    cleanup

--- src/hip_utils.h
+++ src/hip_utils.h
@@ -20,20 +20,6 @@ static inline void gpuAssert(const char *file, int line){
    }
 }
 
-
-// /// \brief Checks if the provided error code is \p hipSuccess and if not,
-// /// prints an error message to the standard error output and terminates the program
-// /// with an error code.
-// #define HIP_CHECK(condition)                                                                \
-//     {                                                                                       \
-//         const hipError_t error = (condition);                                                 \
-//         if(error != hipSuccess)                                                             \
-//         {                \
-//             fprintf(stderr,"HIP error: %s \n function: %s, in file: %s, line number: %d\n", hipGetErrorString(error),__func__,__FILE__, __LINE__);                               \
-//             exit(error_exit_code);                                                          \
-//         }                                                                                   \
-//     }
-
 // https://stackoverflow.com/questions/17399119/how-do-i-use-atomicmax-on-floating-point-values-in-cuda
 __device__ __forceinline__ static float atomicMaxFloat (float * addr, float value) {
     float old;
