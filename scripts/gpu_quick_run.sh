d6c3d9d7e34fd1a3164dcb0b2c39bfe3099f2905
Bonson Wong <boson.ym@gmail.com>
2024-11-22 13:25:58 +1100

    add pawsey path

--- scripts/gpu_quick_run.sh
+++ scripts/gpu_quick_run.sh
@@ -44,6 +44,7 @@ fi
 
 DATA_DIR=/data/bonwon/slorado_test_data/blobs
 # DATA_DIR=/data/bonson/slorado_test_data/blobs
+# DATA_DIR=/software/projects/pawsey1099/bonwon/slorado_test_data/blobs
 SCORES=${DATA_DIR}/${MODEL}_${BATCH_SIZE}c_scores_TNC_half.blob
 
 /usr/bin/time  --verbose ./openfish ${SCORES} models/dna_r10.4.1_e8.2_400bps_${MODEL}@v4.2.0 ${BATCH_SIZE} ${STATE_LEN} || die "tool failed"
