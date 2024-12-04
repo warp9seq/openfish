ad1bba89ddc5f674c0082fef320253a7aba0223f
Bonson Wong <boson.ym@gmail.com>
2024-12-04 13:40:52 +1100

    rm tim for test

--- test/test.sh
+++ test/test.sh
@@ -30,7 +30,7 @@ INTENS_LEN=$(( BATCH_SIZE*(TIMESTEPS) ))
 
 SCORES=test/data/scores.blob
 
-ex /usr/bin/time  --verbose ./openfish ${SCORES} ${BATCH_SIZE} ${STATE_LEN} || die "tool failed"
+ex ./openfish ${SCORES} ${BATCH_SIZE} ${STATE_LEN} || die "tool failed"
 
 diff moves.blob test/data/moves.blob || die "tool failed"
 diff sequence.blob test/data/sequence.blob || die "tool failed"
