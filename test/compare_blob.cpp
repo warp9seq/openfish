8decf53f6fa31e5d9bbf23d6fb8020d761306800
BonsonW <bonson.ym@gmail.com>
2025-04-24 08:37:22 +1000

    better compare

--- test/compare_blob.cpp
+++ test/compare_blob.cpp
@@ -7,19 +7,25 @@
 // g++ -o compare_blob test/compare_blob.cpp
 
 int main(int argc, char* argv[]) {
-    if (argc != 4) {
+    if (argc != 3) {
         fprintf(stderr, "%s\n", "usage: compare_blob <tens_0.blob> <tens_1.blob>");
     }
     FILE *fp;
     size_t result;
 
-    fprintf(stderr, "comapring files %s and %s\n", argv[1], argv[2]);
+    fprintf(stderr, "comparing files %s and %s\n", argv[1], argv[2]);
 
-    size_t tens_len = strtol(argv[3], NULL, 10);
+    size_t tens_len;
+    fp = fopen(argv[1], "rb");
+    fseek(fp, 0, SEEK_END);
+    tens_len = ftell(fp) / sizeof(float);
+    rewind(fp);
+
+    fprintf(stderr, "tens len: %zu\n", tens_len);
+
     float *tens0 = (float *)calloc(tens_len, sizeof(float));
     float *tens1 = (float *)calloc(tens_len, sizeof(float));
 
-    fp = fopen(argv[1], "rb");
     result = fread(tens0, sizeof(float), tens_len, fp);
     if (result != tens_len) {
         fprintf(stderr, "%s\n", "error reading file 0");
@@ -31,6 +37,11 @@ int main(int argc, char* argv[]) {
     result = fread(tens1, sizeof(float), tens_len, fp);
     if (result != tens_len) {
         fprintf(stderr, "%s\n", "error reading file 1");
+        exit(1);
+    }
+
+    if (feof(fp) != 0) {
+        fprintf(stderr, "%s\n", "file 0 mismatch len with file 1");
         exit(1);
     }
     fclose(fp);
