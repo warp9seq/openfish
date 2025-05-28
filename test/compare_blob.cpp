10c82356b82e1bf3b94bb649f4f529fd4eb8e9da
BonsonW <bonson.ym@gmail.com>
2025-05-28 16:13:37 +1000

    update compare blob

--- test/compare_blob.cpp
+++ test/compare_blob.cpp
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <math.h>
 #include <cstdint>
+#include <float.h>
 
 // compile me for tests:
-// g++ -o compare_blob test/compare_blob.c 
+// g++ -o compare_blob test/compare_blob.cpp
 
 int main(int argc, char* argv[]) {
     if (argc != 4) {
@@ -36,8 +37,17 @@ int main(int argc, char* argv[]) {
 
     float max_diff = 0.0f;
     float avg_diff = 0.0f;
+    float max_val_0 = -FLT_MAX;
+    float min_val_0 = FLT_MAX;
+    float max_val_1 = -FLT_MAX;
+    float min_val_1 = FLT_MAX;
     uint64_t n_diff = 0;
     for (int i = 0; i < tens_len; ++i) {
+        if (tens0[i] > max_val_0) max_val_0 = tens0[i];
+        if (tens1[i] > max_val_1) max_val_1 = tens1[i];
+        if (tens0[i] < min_val_0) min_val_0 = tens0[i];
+        if (tens1[i] < min_val_1) min_val_1 = tens1[i];
+
         float diff = fabs(tens0[i] - tens1[i]);
         if (diff != 0.0f) {
             if (diff > max_diff) {
@@ -49,6 +59,7 @@ int main(int argc, char* argv[]) {
     }
     avg_diff /= tens_len;
     fprintf(stderr, "tensor max elem diff by %.32f, avg diff: %f, tens_len: %zu, n_diffs: %zu\n", max_diff, avg_diff, tens_len, n_diff);
+    fprintf(stderr, "min_val_0: %.3f, max_val_0: %.3f, min_val_1: %.3f, max_val_1: %.3f\n", min_val_0, max_val_0, min_val_1, max_val_1);
 
     return 0;
 }
